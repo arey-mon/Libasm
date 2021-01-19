51a7adadafd9c7c3d0766a59122d69c0781deb3b
apreymon <apreymon@student.42.fr>
2021-01-19 19:12:57 +0100

    libasm_over_needs_norm

--- ft_write.s
+++ ft_write.s
@@ -1,8 +1,6 @@
 ;	ssize_t ft_write(int fd, const void *buf, size_t count)
 ;			 RDI		RSI		RDX
 
-; idea is to mov RSI IN RDI
-
 global ft_write
 extern __errno_location
 section .text
@@ -10,17 +8,14 @@ section .text
 ft_write :
 mov RAX, 1
 syscall
-jl exit
-;error
-;jmp exit
+cmp RAX, 0
+jl error	; if RAX < 0
+ret
 
-;error :
-;mov RAX, -1
-;ret
-
-exit :
-neg RAX
+error :
+neg RAX		; errno values are positive
 mov R8, RAX
 call __errno_location
 mov qword [RAX], R8
+mov RAX, -1
 ret
